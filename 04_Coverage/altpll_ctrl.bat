3bef443e8c375b9a50fd27a9024b95872d01f721
AUDIY <96096729+AUDIY@users.noreply.github.com>
2025-08-17 22:56:15 +0900

    Add files via upload

--- 04_Coverage/altpll_ctrl.bat
+++ 04_Coverage/altpll_ctrl.bat
@@ -2,7 +2,7 @@ vlib work
 
 vmap work work
 
-vlog +cover=bcsef ./altpll_ctrl.v
+vlog +cover=bcsft ./altpll_ctrl.v
 vlog -sv ./altpll_ctrl_tb.sv
 
 vsim -t ns -coverage -voptargs=+acc -debugdb=+acc work.altpll_ctrl_tb -do "do run.do"
