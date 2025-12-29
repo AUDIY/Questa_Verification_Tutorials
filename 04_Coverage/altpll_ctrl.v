e23b398bda5c920e995f7471be5ce9fef9c9ebb8
AUDIY <96096729+AUDIY@users.noreply.github.com>
2025-12-29 22:15:07 +0900

    Update comments and formatting in altpll_ctrl.v

--- 04_Coverage/altpll_ctrl.v
+++ 04_Coverage/altpll_ctrl.v
@@ -21,7 +21,7 @@
 *
 * License under CERN-OHL-P v2
 --------------------------------------------------------------------------------
-| Copyright AUDIY 2025.                                                        |
+| Copyright AUDIY 2025 - 2026.                                                 |
 |                                                                              |
 | This source describes Open Hardware and is licensed under the CERN-OHL-P v2. |
 |                                                                              |
@@ -198,3 +198,4 @@ module altpll_ctrl #(
 endmodule
 
 `default_nettype wire
+
