3a4b18ff92b3c2556f8d5b69af5027b0959f6042
AUDIY <96096729+AUDIY@users.noreply.github.com>
2025-12-29 22:14:05 +0900

    Update copyright year and formatting in multadd_tb.sv

--- 01_GUI/multadd_tb.sv
+++ 01_GUI/multadd_tb.sv
@@ -6,7 +6,7 @@
 *
 * License under CERN-OHL-P v2
 --------------------------------------------------------------------------------
-| Copyright AUDIY 2025.                                                        |
+| Copyright AUDIY 2025 - 2026.                                                 |
 |                                                                              |
 | This source describes Open Hardware and is licensed under the CERN-OHL-P v2. |
 |                                                                              |
@@ -92,4 +92,5 @@ module multadd_tb ();
 endmodule
 
 `default_nettype wire
+
 
