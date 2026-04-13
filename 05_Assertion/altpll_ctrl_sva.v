44f59ccdfa310ca63edfe40de2edd534f9be4433
AUDIY <96096729+AUDIY@users.noreply.github.com>
2026-04-13 18:55:08 +0900

    Update altpll_ctrl_sva.v header and formatting

--- 05_Assertion/altpll_ctrl_sva.v
+++ 05_Assertion/altpll_ctrl_sva.v
@@ -2,7 +2,7 @@
 * altpll_ctrl_sva.v
 *
 *   Author: AUDIY
-*   Date  : 2025/08/19
+*   Date  : 2026/04/13
 *
 *   Inputs
 *       clk      : Clock (ALTPLL slower input clock)
@@ -26,7 +26,7 @@
 | This source describes Open Hardware and is licensed under the CERN-OHL-P v2. |
 |                                                                              |
 | You may redistribute and modify this source and make products using it under |
-| the terms of the CERN-OHL-P v2 (https:/cern.ch/cern-ohl).                    |
+| the terms of the CERN-OHL-P v2 (https://cern.ch/cern-ohl).                   |
 |                                                                              |
 | This source is distributed WITHOUT ANY EXPRESS OR IMPLIED WARRANTY,          |
 | INCLUDING OF MERCHANTABILITY, SATISFACTORY QUALITY AND FITNESS FOR A         |
